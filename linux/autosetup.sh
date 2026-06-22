3d555bb357f84e4ad56f2b31d8dd6dd15898c40e
seoyul710 <seoyulshim710@gmail.com>
2026-06-23 08:59:06 +0900

    kitty.conf 추가

--- linux/autosetup.sh
+++ linux/autosetup.sh
@@ -33,10 +33,12 @@ curl -sS https://starship.rs/install.sh | sh
 
 # config folders
 mkdir -p ~/.config
+mkdir -p ~/.config/kitty
 
 # dotfiles copy
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cp "$SCRIPT_DIR/.zshrc" ~/
+cp "$SCRIPT_DIR/kitty.conf" ~/.config/kitty/kitty.conf
 "$SCRIPT_DIR/switch-profile.sh" apply default
 
 echo "done. relog required"
